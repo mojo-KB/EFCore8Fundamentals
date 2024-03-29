93cfde517636d7b0534d1bdc0716785b11eae8d6
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2024-03-29 19:02:17 -0400

    saved note

--- M02 Building Your FIrst App/Before/PublisherData/Class1.cs
+++ M02 Building Your FIrst App/Before/PublisherData/Class1.cs
@@ -1,9 +1,17 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore;
+using PublisherDomain;
 
-
-namespace PublisherData {
-    public class PubContext : Db
+namespace PublisherData
+{
+    public class PubContext: DbContext 
     {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = PubIntroDatabase");
+        }
     }
 }
