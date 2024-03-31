bdbd4d3f3c26dd5475a843139b2a65b93d4e8c66
Bunla Kour <56409448+mojo-KB@users.noreply.github.com>
2024-03-31 17:10:42 -0400

    saved

--- M02 Building Your FIrst App/Before/PublisherData/Class1.cs
+++ M02 Building Your FIrst App/Before/PublisherData/Class1.cs
@@ -3,15 +3,8 @@ using PublisherDomain;
 
 namespace PublisherData
 {
-    public class PubContext: DbContext 
+    public class PubContext: Dbcontext
     {
-        public DbSet<Author> Authors { get; set; }
-        public DbSet<Book> Books { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(
-                "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = PubIntroDatabase");
-        }
     }
 }
